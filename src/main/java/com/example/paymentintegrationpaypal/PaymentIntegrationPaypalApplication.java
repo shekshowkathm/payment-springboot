0e53d0c428565d66d53c19d244f7efaf06f8dcf0
shekshowkathm <shekshowkath2001ms@gmail.com>
2023-08-30 16:00:30 +0530

    payemnt poc

---
+++ src/main/java/com/example/paymentintegrationpaypal/PaymentIntegrationPaypalApplication.java
@@ -0,0 +1,13 @@
+package com.example.paymentintegrationpaypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentIntegrationPaypalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentIntegrationPaypalApplication.class, args);
+	}
+
+}
